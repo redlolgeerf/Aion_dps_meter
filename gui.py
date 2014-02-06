1e4c7ba98ccb66dd0a7f7ec2116375cc7b6a27f4
redlolgeerf <mihanne@list.ru>
2014-02-06 21:58:56 +0400

    refactored for PEP8

--- gui.py
+++ gui.py
@@ -11,22 +11,21 @@ from dps_counter import test1
 import dps_gui
 
 
-class  Dps_counter(QtGui.QMainWindow, dps_gui.Ui_MainWindow):
-    '''main class'''
+class DpsCounter(QtGui.QMainWindow, dps_gui.Ui_MainWindow):
+    """main class"""
     def __init__(self, parent=None):
-        '''init it'''
-        super(Dps_counter, self).__init__(parent)
+        super(DpsCounter, self).__init__(parent)
         self.setupUi(self)
         self.ui_setup()
         self.config = configparser.ConfigParser()
         self.read_config()
         self.apply_config()
         self.statusBar().showMessage(my_damage_table.file_path)
-        self.connectActions()
+        self.connect_actions()
 
     def ui_setup(self):
         """
-        just some hacks to compensate qtdesigner
+        just some hacks to compensate qt designer
         """
         self.tableWidget.setColumnWidth(0, 200)
         self.skillslist.setColumnWidth(0, 300)
@@ -55,58 +54,58 @@ class  Dps_counter(QtGui.QMainWindow, dps_gui.Ui_MainWindow):
             self.config['DEFAULT_ODDSKILLS'][skill] = '1'
 
     def apply_config(self):
-        '''assignes config to variables'''
+        """assigns config to variables"""
         my_damage_table.file_path = self.config['DEFAULT']['Path']
         my_damage_table.my_oddskills = []
         for skill in self.config['DEFAULT_ODDSKILLS']:
             if self.config['DEFAULT_ODDSKILLS'][skill] == 1:
                 my_damage_table.my_oddskills.append(skill)
 
-    def connectActions(self):
-        '''connect actions to slots'''
+    def connect_actions(self):
+        """connect actions to slots"""
         self.anal_button.clicked[bool].connect(self.recalculate)
-        self.delButton.clicked[bool].connect(self.deleteFile)
-        self.combo.activated[str].connect(self.onActivated)
-        self.open_file.triggered.connect(self.openFile)
+        self.delButton.clicked[bool].connect(self.delete_file)
+        self.combo.activated[str].connect(self.on_activated)
+        self.open_file.triggered.connect(self.open_file_dialog)
         # hot keys
-        QtGui.QShortcut(QtGui.QKeySequence("Ctrl+D"), self, self.deleteFile)
-        QtGui.QShortcut(QtGui.QKeySequence("Ctrl+В"), self, self.deleteFile)  # for Russian
-        QtGui.QShortcut(QtGui.QKeySequence("Ctrl+O"), self, self.openFile)
-        QtGui.QShortcut(QtGui.QKeySequence("Ctrl+Щ"), self, self.openFile)  # for Russian
+        QtGui.QShortcut(QtGui.QKeySequence("Ctrl+D"), self, self.delete_file)
+        QtGui.QShortcut(QtGui.QKeySequence("Ctrl+В"), self, self.delete_file)  # for Russian
+        QtGui.QShortcut(QtGui.QKeySequence("Ctrl+O"), self, self.open_file_dialog)
+        QtGui.QShortcut(QtGui.QKeySequence("Ctrl+Щ"), self, self.open_file_dialog)  # for Russian
         QtGui.QShortcut(QtGui.QKeySequence("Ctrl+G"), self, self.recalculate)
         QtGui.QShortcut(QtGui.QKeySequence("Ctrl+П"), self, self.recalculate)  # for Russian
-        self.tableWidget.customContextMenuRequested.connect(self.handleTableMenu)
+        self.tableWidget.customContextMenuRequested.connect(self.handle_table_menu)
 
-    def handleTableMenu(self, pos):
-        'context menu for charactertable'
+    def handle_table_menu(self, pos):
+        """context menu for charactertable"""
         self.menu = QtGui.QMenu(self)
-        showDetail = QtGui.QAction('Детали', self)
-        self.menu.addAction(showDetail)
+        show_detail = QtGui.QAction('Детали', self)
+        self.menu.addAction(show_detail)
         index = self.tableWidget.indexAt(pos)
         if index.row() > -1:
             action = self.menu.exec_(QtGui.QCursor.pos())
-            if action == showDetail:
-                self.showDetail(index.row())
+            if action == show_detail:
+                self.show_detail(index.row())
         return
 
-    def showDetail(self, row):
-        'action for character table context menu, switches to detail tab'
+    def show_detail(self, row):
+        """action for character table context menu, switches to detail tab"""
         characters = sorted(my_damage_table.damage_dealt.keys())
         character = self.tableWidget.item(row, 0).text()
-        self.fillSkillsList(character)
+        self.fill_skills_list(character)
         self.combo.setCurrentIndex(characters.index(character))
         self.tabWidget.setCurrentIndex(1)
 
     def main(self):  # lint:ok
         self.show()
 
-    def onActivated(self, text):
-        '''when combo box is touched skills list is ammended'''
+    def on_activated(self, text):
+        """when combo box is touched skills list is amended"""
         character = text
-        self.fillSkillsList(character)
+        self.fill_skills_list(character)
 
-    def fillSkillsList(self, character=None):
-        '''just fills skillslist'''
+    def fill_skills_list(self, character=None):
+        """just fills skills list"""
         if my_damage_table.damage_dealt:
             i = 0
             skills = sorted(my_damage_table.damage_dealt[character])
@@ -117,11 +116,9 @@ class  Dps_counter(QtGui.QMainWindow, dps_gui.Ui_MainWindow):
                     damage = my_damage_table.damage_dealt[character][skill][0]
                     strikes = my_damage_table.damage_dealt[character][skill][1]
                     commit = damage / my_damage_table\
-                    .damage_dealt[character]['total_damage'][0] * 100
-                    for x in range(4):
-                        item = QtGui.QTableWidgetItem()
+                        .damage_dealt[character]['total_damage'][0] * 100
                     for x, value in zip(list(range(5)), (skill,
-                    damage, strikes, int(damage / strikes), commit)):
+                                        damage, strikes, int(damage / strikes), commit)):
                         item = QtGui.QTableWidgetItem()
                         item.setData(QtCore.Qt.DisplayRole, value)
                         self.skillslist.setItem(i, x, item)
@@ -132,14 +129,14 @@ class  Dps_counter(QtGui.QMainWindow, dps_gui.Ui_MainWindow):
             strikes = my_damage_table.damage_dealt[character]['total_damage'][1]
 
             for x, value in zip(list(range(5)), ('Всего', damage, strikes,
-            int(damage / strikes), 100)):
+                                int(damage / strikes), 100)):
                 item = QtGui.QTableWidgetItem()
                 item.setData(QtCore.Qt.DisplayRole, value)
                 self.skillslist.setItem(i, x, item)
             self.skillslist.setSortingEnabled(True)
 
-    def fillCharacterList(self):
-        '''just fills characterlist'''
+    def fill_character_list(self):
+        """just fills character list"""
         if my_damage_table.damage_dealt:
             i = 0
             if not test1(my_damage_table.damage_dealt):
@@ -150,24 +147,24 @@ class  Dps_counter(QtGui.QMainWindow, dps_gui.Ui_MainWindow):
             self.tableWidget.setRowCount(len(characters))
             self.tableWidget.clearContents()
             if my_damage_table.damage_dealt.get('Вы'):
-                self.fillCharacterrow('Вы', i)
+                self.fill_character_row('Вы', i)
                 i += 1
             for character_ in characters:
                 if character_ != 'Вы':
-                    self.fillCharacterrow(character_, i)
+                    self.fill_character_row(character_, i)
                     i += 1
             self.tableWidget.setSortingEnabled(True)
         return
 
-    def fillCharacterrow(self, character, i):
-        '''to allow tailore filling of the table'''
+    def fill_character_row(self, character, i):
+        """to allow tailor filling of the table"""
         damage = my_damage_table.damage_dealt[character]['total_damage'][0]
         total_all = 0
         for character_ in my_damage_table.damage_dealt:
             total_all += my_damage_table.damage_dealt[character_]['total_damage'][0]
         commit = (damage / total_all * 100)
         times = int((my_damage_table.timing[character]['ended'] -
-        my_damage_table.timing[character]['started']).total_seconds())
+                    my_damage_table.timing[character]['started']).total_seconds())
         if times:
             dps = int(damage / times)
         else:
@@ -178,7 +175,7 @@ class  Dps_counter(QtGui.QMainWindow, dps_gui.Ui_MainWindow):
                 crit = int(my_damage_table.crites[character]['crit'] /
                 my_damage_table.crites[character]['strikes'] * 100)
         for x, value in zip(list(range(6)), (character, damage, commit,
-        dps, crit, self.timeConvert(times))):
+                            dps, crit, self.time_convert(times))):
             item = QtGui.QTableWidgetItem()
             item.setData(QtCore.Qt.DisplayRole, value)
             if character == 'Вы':
@@ -186,8 +183,8 @@ class  Dps_counter(QtGui.QMainWindow, dps_gui.Ui_MainWindow):
             self.tableWidget.setItem(i, x, item)
         return
 
-    def timeConvert(self, time, *args):
-        'takes time and converts it in words with right endings'
+    def time_convert(self, time, *args):
+        """takes time and converts it in words with right endings"""
         minutes = int(time / 60) if time > 0 else 0
         seconds = time - 60 * minutes
         if minutes % 20 == 1:
@@ -200,9 +197,13 @@ class  Dps_counter(QtGui.QMainWindow, dps_gui.Ui_MainWindow):
             minutes = '%d минут%s' % (minutes, mending)
         else:
             minutes = ''
-        if seconds % 20 == 1:
+        if seconds < 21:
+            d = 20
+        else:
+            d = 10
+        if seconds % d == 1:
             sending = 'у'
-        elif 1 < seconds % 20 < 5:
+        elif 1 < seconds % d < 5:
             sending = 'а'
         else:
             sending = ''
@@ -211,38 +212,38 @@ class  Dps_counter(QtGui.QMainWindow, dps_gui.Ui_MainWindow):
         else:
             seconds = ''
         return ('%s, %s' % (minutes, seconds) if (minutes and seconds)
-        else '%s%s' % (minutes, seconds))
+                else '%s%s' % (minutes, seconds))
 
-    def fillcombo(self):
-        '''fills combo with all characters'''
+    def fill_combo(self):
+        """fills combo with all characters"""
         self.combo.clear()
         if my_damage_table.damage_dealt:
             for character_ in sorted(list(my_damage_table.damage_dealt.keys()), key=str.lower):
                 self.combo.addItem(character_)
 
-    def openFile(self):
-        '''a dialog box to open log file'''
+    def open_file_dialog(self):
+        """a dialog box to open log file"""
         self.config['DEFAULT']['Path'] = QtGui.QFileDialog.getOpenFileName(self,
-        'Open file', '/home/eyeinthebrick/Python/dps')
+                                        'Open file', '/home/eyeinthebrick/Python/dps')
         self.apply_config()
         self.save_config()
         self.statusBar().showMessage(my_damage_table.file_path)
 
-    def deleteFile(self):
-        '''just to be short'''
+    def delete_file(self):
+        """just to be short"""
         with open(my_damage_table.file_path, 'w', encoding='cp1251') as f:
             f.write('')
         self.tableWidget.setRowCount(0)
 
     def recalculate(self):
-        '''recalculates all values in my_damage_table based on data from opened file'''
+        """recalculates all values in my_damage_table based on data from opened file"""
         self.anal_button.setText('Считаю')
         if not my_damage_table.read_file():
             reply = QtGui.QMessageBox.question(self, 'Не найден лог файл',
-            "Хотите выбрать лог файл?", QtGui.QMessageBox.Yes |
-            QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+                                               "Хотите выбрать лог файл?", QtGui.QMessageBox.Yes |
+                                                QtGui.QMessageBox.No, QtGui.QMessageBox.No)
             if reply == QtGui.QMessageBox.Yes:
-                self.openFile()
+                self.open_file_dialog()
                 self.recalculate()
         else:
             #print('threading!')
@@ -256,24 +257,24 @@ class  Dps_counter(QtGui.QMainWindow, dps_gui.Ui_MainWindow):
 
             self.go = True
             self.progressBar.setRange(0, 0)
-            while self.go:  # dirty hack, chage to something more safe
+            while self.go:  # dirty hack, change to something more safe
                 QtGui.QApplication.processEvents()
             self.progressBar.setRange(0, 1)
             self.anal_button.setText('Посчитать')
-            #print('continueing!')
+            #print('continuing!')
             if not my_damage_table.damage_dealt:
                 QtGui.QMessageBox.information(self, 'Лог файл пуст',
-                "Лог файл пуст", QtGui.QMessageBox.Ok)
+                                                "Лог файл пуст", QtGui.QMessageBox.Ok)
                 return
 
             character = sorted(my_damage_table.damage_dealt.keys(), key=str.lower)[0]
-            self.fillcombo()
-            self.fillSkillsList(character)
-            self.fillCharacterList()
+            self.fill_combo()
+            self.fill_skills_list(character)
+            self.fill_character_list()
 
 
 class FillingThread(QtCore.QThread):
-    'thead to calculate'
+    """thead to calculate"""
     def __init__(self):  # lint:ok
         QtCore.QThread.__init__(self)
 
@@ -282,12 +283,12 @@ class FillingThread(QtCore.QThread):
         Dps_counter.go = False
 
     def __del__(self):
-        '''implemented, so the tread is not destroyed by garbage collector,
-        while working'''
+        """implemented, so the tread is not destroyed by garbage collector,
+        while working"""
         self.wait()
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
-    Dps_counter = Dps_counter()
+    Dps_counter = DpsCounter()
     Dps_counter.main()
     app.exec_()
